9b539bef75dd671a21b4ddea6cb959436d493382
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2023-04-18 13:59:44 +0200

    Added missing token failsafe

--- src/main/java/bora/App.java
+++ src/main/java/bora/App.java
@@ -2,18 +2,35 @@ package bora;
 
 import gpt.api.GPT;
 import gpt.chat.Chat;
+import utils.Printer;
 import utils.PropertyUtility;
+
+import java.util.Scanner;
 
 /**
  * Chat!
  *
  */
 public class App {
+    static Printer log = new Printer(App.class);
     public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
         PropertyUtility.loadProperties("src/test/resources/test.properties");
-        String token = PropertyUtility.properties.getProperty("gpt-token");
-        GPT gpt = new GPT(token);
-        Chat chat = new Chat(gpt);
-        chat.startChat();
+        try{
+            String token = PropertyUtility.properties.getProperty("gpt-token", "");
+            if (token.trim().isEmpty()) token = getToken(scanner);
+            GPT gpt = new GPT(token);
+            Chat chat = new Chat(gpt);
+            chat.startChat(scanner);
+            scanner.close();
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+
+    static String getToken(Scanner scanner){
+        log.new Info("Please enter your token:");
+        String token = scanner.nextLine();
+        log.new Info("Thanks!");
+        return token;
     }
 }
