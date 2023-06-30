526ea73e7c7ec0f213a6562b274569cbf5c56e34
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2023-06-30 20:48:00 +0200

    GUI added!

--- src/main/java/bora/App.java
+++ src/main/java/bora/App.java
@@ -1,11 +1,14 @@
 package bora;
 
 import gpt.api.GPT;
-import gpt.chat.Chat;
+import gpt.chat.ui.ChatGUI;
+import gpt.chat.ui.ChatGUIFactory;
 import utils.Printer;
 import utils.PropertyUtility;
 
 import java.util.Scanner;
+
+import static gpt.chat.ui.ChatGUIFactory.Theme.light;
 
 /**
  * Chat!
@@ -14,23 +17,20 @@ import java.util.Scanner;
 public class App {
     static Printer log = new Printer(App.class);
     public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in);
+
         PropertyUtility.loadProperties("src/test/resources/test.properties");
         try{
-            String token = PropertyUtility.properties.getProperty("gpt-token", "");
-            if (token.trim().isEmpty()) token = getToken(scanner);
+            String token = PropertyUtility.getProperty("gpt-token", "");
             GPT gpt = new GPT(token);
-            Chat chat = new Chat(gpt);
-            chat.startChat(scanner);
-            scanner.close();
+            ChatGUI chat = ChatGUIFactory.getChatGUI(
+                    light,
+                    gpt
+            );
+            chat.setTemperature(Double.parseDouble(PropertyUtility.getProperty("temperature", "0.5")));
+            chat.start();
         }
         catch (Exception e){e.printStackTrace();}
     }
 
-    static String getToken(Scanner scanner){
-        log.new Info("Please enter your token:");
-        String token = scanner.nextLine();
-        log.new Info("Thanks!");
-        return token;
-    }
+
 }
